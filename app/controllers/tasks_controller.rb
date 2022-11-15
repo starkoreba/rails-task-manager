f179c2e80f8cdcc3f4004cb4ced776deeae1d74a
Maeva De Campos <kulwilcoox@gmail.com>
2022-11-15 13:14:53 +0100

    done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,11 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :destroy]
+
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def new
@@ -18,9 +19,19 @@ class TasksController < ApplicationController
     redirect_to task_path(@task)
   end
 
+  def destroy
+    @task.destroy
+    # No need for app/views/tasks/destroy.html.erb
+    redirect_to tasks_path, status: :see_other
+  end
+
   private
 
   def task_params
     params.require(:task).permit(:title, :details)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :tasks, only: [:create, :index, :destroy]
   # Defines the root path route ("/")
   # root "articles#index"
   get 'tasks', to: 'tasks#index'
   get 'tasks/new', to: 'tasks#new', as: 'new_task'
   get 'tasks/:id', to: 'tasks#show', as: 'task'
   post 'tasks', to: 'tasks#create'
+  delete 'tasks/:id/delete', to: 'tasks#destroy'
 end
